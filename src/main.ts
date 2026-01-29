3f6b0d83de60157a3d9f5b678e2207106b4313ff
anyvagrant <noobheeh@gmail.com>
2026-01-29 18:55:03 -0300

    main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideZoneChangeDetection } from '@angular/core';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    ...appConfig.providers,
     provideZoneChangeDetection({ eventCoalescing: true })
   ]
 }).catch((err) => console.error(err));
