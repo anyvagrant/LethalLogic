a49470d06572791897fe9d800c2bd1de010c1d2b
anyvagrant <noobheeh@gmail.com>
2026-01-29 21:08:53 -0300

    main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
+import { AppComponent } from './app.component'; // MUDANÇA AQUI: sem a pasta /app/
 import { provideZoneChangeDetection } from '@angular/core';
 
 bootstrapApplication(AppComponent, {
